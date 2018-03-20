b774e1bb70439b7d31e98f56378b525ba3d301a0
maximest-pierre <me@maximest-pierre.me>
2018-03-19 22:29:30 -0400

    Cleanup and finishing up yt_sync features

---
+++ web/js/yt2/youtube_video.js
@@ -0,0 +1,26 @@
+
+function playVideo1(){
+    var myVideo = document.getElementById("video1");
+
+    myVideo.play();
+    $('#video1').prop("controls", true);
+    $('#play-video1').hide();
+}
+
+function playVideo2(){
+    var myVideo = document.getElementById("video2");
+
+    myVideo.play();
+    $('#video2').prop("controls", true);
+    $('#play-video2').hide();
+
+}
+
+function playVideo3() {
+    var myVideo = document.getElementById("video3");
+
+    myVideo.play();
+    $('#video3').prop("controls", true);
+    $('#play-video3').hide();
+
+}
